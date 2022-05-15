74f49e88daa116961d6ca81141cc4d11051e811b
何航 <hehang724590957@qq.com>
2022-05-15 13:07:24 +0800

    add scrcpy sub module

--- app/src/main/java/com/picapico/scrcpyhttp/Server.java
+++ app/src/main/java/com/picapico/scrcpyhttp/Server.java
@@ -2,6 +2,7 @@ package com.picapico.scrcpyhttp;
 
 public class Server {
     public static void main(String... args) throws Exception {
+        com.genymobile.scrcpy.Server.main(args);
 
     }
 }
